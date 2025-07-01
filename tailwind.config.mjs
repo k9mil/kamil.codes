cb34e8b7c1947acb3659c30f13c3afef4ea4e554
Kamil Zak <kamilzak00@gmail.com>
2025-07-01 19:33:54 +0100

    chore: update deps & format with prettier

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,32 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 
-const colors = require('tailwindcss/colors')
-const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-		  colors: {
-			transparent: 'transparent',
-			current: 'currentColor',
-			black: colors.black,
-			white: colors.white,
-			'neutral': {
-			  50: '#fafafa',
-			  100: '#f5f5f5',
-			  200: '#e5e5e5',
-			},
-		  },
-		},
-		fontFamily: {
-		  'inter': ['inter']
-		},
-		screens: {
-		  'xxs': '1px',
-		  'xs': '475px',
-		  ...defaultTheme.screens,
-		},
-	  },
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        black: colors.black,
+        white: colors.white,
+        neutral: {
+          50: "#fafafa",
+          100: "#f5f5f5",
+          200: "#e5e5e5",
+        },
+      },
+    },
+    fontFamily: {
+      inter: ["inter"],
+    },
+    screens: {
+      xxs: "1px",
+      xs: "475px",
+      ...defaultTheme.screens,
+    },
+  },
+  plugins: [],
+};
